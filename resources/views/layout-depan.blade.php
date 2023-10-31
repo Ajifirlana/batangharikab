31e7743894b77218c9caf3615a757bc980dd303f
Ajifirlana <firlana89@gmail.com>
2023-10-31 17:20:03 +0700

    updt

--- resources/views/layout-depan.blade.php
+++ resources/views/layout-depan.blade.php
@@ -203,14 +203,24 @@
                             <div class="col-md-4">
                                 <div class="features_item sm-m-top-30">
                                     <div class="f_item_text">
-                                        <h3>Profil SKPD</h3>
-                                        <p>
+                                        <h3><i class="fa fa-arrow-circle-right"></i>Profil SKPD</h3>
+                                   
+<div class="card">
+<div class="profil-skpd">
+    <!-- <p>
         <ul>
             <li class="dropdown">Sekretariat
              <ul><li><a target="_blank" href="https://setda.batangharikab.go.id/">SEKRETARIAT DAERAH</a></li><li><a target="_blank" href="http://dprd.batangharikab.go.id/">SEKRETARIAT DPRD</a></li></ul></li><li class='category'>Dinas
              <ul><li><a target="_blank" href="http://dpdk.batangharikab.go.id/">DINAS PERPUSTAKAAN DAN KEARSIPAN</a></li><li><a target="_blank" href="http://bkbpp.batangharikab.go.id/">DINAS PENGENDALIAN PENDUDUK, KELUARGA BERENCANA, PEMBERDAYAAN PEREMPUAN DAN PERLINDUNGAN ANAK</a></li><li><a target="_blank" href="https://disnakertrans.batangharikab.go.id/">DINAS TENAGA KERJA DAN TRANSMIGRASI</a></li><li><a target="_blank" href="http://diskominfo.batangharikab.go.id/">DINAS KOMUNIKASI DAN INFORMATIKA</a></li><li><a target="_blank" href="http://disbunnak.batangharikab.go.id/">DINAS PERKEBUNAN DAN PETERNAKAN</a></li><li><a target="_blank" href="http://dishub.batangharikab.go.id/">DINAS PERHUBUNGAN</a></li><li><a target="_blank" href="http://dinsos.batangharikab.go.id/">DINAS SOSIAL</a></li><li><a target="_blank" href="http://dpmd.batangharikab.go.id/">DINAS PEMBERDAYAAN MASYARAKAT DAN DESA</a></li><li><a target="_blank" href="http://disdukcapil.batangharikab.go.id/">DINAS KEPENDUDUKAN DAN PENCATATAN SIPIL</a></li><li><a target="_blank" href="http://damkar.batangharikab.go.id/">DINAS PEMADAM KEBAKARAN DAN PENYELAMATAN</a></li><li><a target="_blank" href="https://pdk.batangharikab.go.id/">DINAS PENDIDIKAN DAN KEBUDAYAAN</a></li><li><a target="_blank" href="http://perkim.batangharikab.go.id/">DINAS PERUMAHAN DAN KAWASAN PEMUKIMAN</a></li><li><a target="_blank" href="http://dinkes.batangharikab.go.id/">DINAS KESEHATAN</a></li><li><a target="_blank" href="http://pupr.batangharikab.go.id/">DINAS  PEKERJAAN UMUM DAN PENATAAN RUANG</a></li><li><a target="_blank" href="http://dpmptsp.batangharikab.go.id/">DINAS PENANAMAN MODAL DAN PELAYANAN TERPADU SATU PINTU</a></li><li><a target="_blank" href="http://dispora.batangharikab.go.id/">DINAS PEMUDA DAN OLAHRAGA</a></li><li><a target="_blank" href="http://dinaslh.batangharikab.go.id/">DINAS LINGKUNGAN HIDUP</a></li><li><a target="_blank" href="http://diskankp.batangharikab.go.id/">DINAS PERIKANAN DAN KETAHANAN PANGAN</a></li><li><a target="_blank" href="http://dtph.batangharikab.go.id/">DINAS TANAMAN PANGAN DAN HORTIKULTURA</a></li><li><a target="_blank" href="http://diskoperindag.batangharikab.go.id/">DINAS KOPERASI PERINDUSTRIAN DAN PERDAGANGAN</a></li></ul></li><li class='category'>Badan
              <ul><li><a target="_blank" href="http://satpolpp.batangharikab.go.id/">SATUAN POLISI PAMONG PRAJA</a></li><li><a target="_blank" href="http://bpbd.batangharikab.go.id/">BADAN PENANGGULANGAN BENCANA DAERAH</a></li><li><a target="_blank" href="http://bkpsdmd.batangharikab.go.id/">BADAN KEPEGAWAIAN DAN PENGEMBANGAN SUMBER DAYA MANUSIA DAERAH</a></li><li><a target="_blank" href="http://bappeda.batangharikab.go.id/">BADAN PERENCANAAN PEMBANGUNAN DAERAH</a></li><li><a target="_blank" href="http://bakeuda.batangharikab.go.id/">BADAN KEUANGAN DAERAH</a></li><li><a target="_blank" href="http://balitbangda.batangharikab.go.id/">BADAN PENELITIAN DAN PENGEMBANGAN DAERAH</a></li><li><a target="_blank" href="http://inspektorat.batangharikab.go.id/">INSPEKTORAT DAERAH</a></li></ul></li><li class='category'>Kantor
-             <ul><li><a target="_blank" href="http://kesbangpol.batangharikab.go.id/">KESATUAN BANGSA DAN POLITIK KABUPATEN BATANG HARI</a></li><li><a target="_blank" href="http://rsudhamba.batangharikab.go.id/">RUMAH SAKIT UMUM DAERAH HAMBA</a></li></ul></li></ul></p>
+             <ul><li><a target="_blank" href="http://kesbangpol.batangharikab.go.id/">KESATUAN BANGSA DAN POLITIK KABUPATEN BATANG HARI</a></li><li><a target="_blank" href="http://rsudhamba.batangharikab.go.id/">RUMAH SAKIT UMUM DAERAH HAMBA</a></li></ul></li></ul>
+            </p> -->
+           <p> <img src="logo/lakip.png" alt="Avatar" style="width:100%"></p>
+           <p><img src="logo/lakip-skpd.jpg" alt="Avatar" style="width:100%"></p>
+           <p><img src="logo/portal-app.png" alt="Avatar" style="width:100%"></p>
+                  
+</div>
+</div>
                                     </div>
                                 </div>
                             </div>
@@ -218,19 +228,25 @@
                                 <div class="features_item sm-m-top-30">
                                    
                                     <div class="f_item_text">
-                                        <h3>BERITA DAERAH</h3>
+                                        <h3><i class="fa fa-arrow-circle-right"></i>BERITA DAERAH</h3>
+                                        <div class="card">
+<div class="profil-skpd">
                                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque eleifend
                                             in sit amet mattis volutpat rhoncus.</p>
-                                    </div>
+</div></div>
+                                        </div>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="features_item sm-m-top-30">
                                    
                                     <div class="f_item_text">
-                                        <h3>Easy to Customize</h3>
+                                        <h3><i class="fa fa-arrow-circle-right"></i>Easy to Customize</h3>
+                                        <div class="card">
+<div class="profil-skpd">
                                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque eleifend
                                             in sit amet mattis volutpat rhoncus.</p>
+</div></div>
                                     </div>
                                 </div>
                             </div>
@@ -241,59 +257,7 @@
 
 
             <!--Business Section-->
-            <section id="business" class="business bg-grey roomy-70">
-                <div class="container">
-                    <div class="row">
-                        <div class="main_business">
-                            <div class="col-md-6">
-                                <div class="business_slid">
-                                    <div class="slid_shap bg-grey"></div>
-                                    <div class="business_items text-center">
-                                        <div class="business_item">
-                                            <div class="business_img">
-                                                <img src="frontend/assets/images/about-img1.jpg" alt="" />
-                                            </div>
-                                        </div>
-
-                                        <div class="business_item">
-                                            <div class="business_img">
-                                                <img src="frontend/assets/images/about-img1.jpg" alt="" />
-                                            </div>
-                                        </div>
-
-                                        <div class="business_item">
-                                            <div class="business_img">
-                                                <img src="frontend/assets/images/about-img1.jpg" alt="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-6">
-                                <div class="business_item sm-m-top-50">
-                                    <h2 class="text-uppercase"><strong>Made</strong> is Template For Business</h2>
-                                    <ul>
-                                        <li><i class="fa fa-arrow-circle-right"></i> Clean & Modern Design</li>
-                                        <li><i class="fa  fa-arrow-circle-right"></i> Fully Responsive</li>
-                                        <li><i class="fa  fa-arrow-circle-right"></i> Google Fonts</li>
-                                    </ul>
-                                    <p class="m-top-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit pellentesque eleifend in mi 
-                                        sit amet mattis suspendisse ac ligula volutpat nisl rhoncus sagittis cras suscipit 
-                                        lacus quis erat malesuada lobortis eiam dui magna volutpat commodo eget pretium vitae
-                                        elit etiam luctus risus urna in malesuada ante convallis.</p>
-
-                                    <div class="business_btn">
-                                        <a href="https://bootstrapthemes.co" class="btn btn-default m-top-20">Read More</a>
-                                        <a href="" class="btn btn-primary m-top-20">Buy Now</a>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section><!-- End off Business section -->
+            <!-- End off Business section -->
 
 
             <!--product section-->
