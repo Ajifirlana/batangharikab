608f06258c87cb9c7bb3e5ccf4629d72b774c611
Ajifirlana <firlana89@gmail.com>
2023-11-02 16:52:08 +0700

    updt

--- resources/views/frontend/jadwal_event.blade.php
+++ resources/views/frontend/jadwal_event.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-4">
                                 
                                     <div class="features_item sm-m-top-30">
-                                        <div class="f_item_icon">
+                                       
                                         </div>
                                         <div class="f_item_text">
                                             <h3>JADWAL EVENT</h3>
@@ -9,6 +9,5 @@
                                                 in sit amet mattis volutpat rhoncus.</p>
                                         </div>
                                     </div>
-                                    </div>
-                                
+                                    
                             </div>
--- resources/views/frontend/video_kegiatan.blade.php
+++ resources/views/frontend/video_kegiatan.blade.php
@@ -1,8 +1,18 @@
-<div class="col-md-6">
-                                <div class="action_btn text-center sm-text-center">
-                                    <h3>VIDEO KEGIATAN PEMERINTAH KAB. BATANG HARI</h3>
-                                    <div class = "caption">
-                                        VIDEO: BUPATI BATANGHARI HADIRI HUT PGRI TINGKAT KAB. BATANG HARI
-                                        </div></div>
-                                
-                            </div>
+
+<div class="col-md-3">
+<div class="widget_latst_item m-top-30">
+                                <h3>VIDEO KEGIATAN PEMERINTAH KAB. BATANG HARI</h3>
+                                   
+                                </div><!-- End off widget item -->
+                                </div>
+<div class="col-md-4">
+                                  
+                                  <div class="widget_latst_item m-top-30">
+                                        <div class="item_icon"><img src="frontend/assets/images/ltst-img-1.jpg" alt="" /></div>
+                                        <div class="widget_latst_item_text">
+                                            <p>VIDEO: BUPATI BATANGHARI HADIRI HUT PGRI TINGKAT KAB. BATANG HARI</p>
+                                            <a href="">21<sup>th</sup> July 2016</a>
+                                        </div>
+                                    </div>
+                                   
+                                </div><!-- End off widget item -->
