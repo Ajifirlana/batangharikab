d2364513e9cc592794a4c0c183e8967c6955962c
Ajifirlana <firlana89@gmail.com>
2023-12-22 13:58:18 +0700

    Update galeri_foto.blade.php

--- resources/views/frontend/galeri/galeri_foto.blade.php
+++ resources/views/frontend/galeri/galeri_foto.blade.php
@@ -1,37 +1,43 @@
 @extends('frontend.layouts.app')
 @section('content')
 
-<section id="action" class="business bg-grey roomy-40">
-</section>
-<section id="action" class="business bg-grey roomy-40">
-                <div class="container">
+        <!--Team One Start-->
+        <section class="team-one">
+            <div class="container">
+                <div class="section-title text-center">
+                    <h2 class="section-title__title">GALERI FOTO
+                        </h2>
+                </div>
+                <div class="row">
+                    <!--Team One Single Start-->
                     
-                <div class="head_title text-center fix">
-                    <h2 class="text-uppercase"><strong>GALERI FOTO</strong></h2>    
-                            
+                    @foreach($galeri_foto as $row)  
+                    <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
+                        <div class="team-one__single">
+                            <div class="team-one__img-box">
+                                <div class="team-one__img">
+                                    <img src="{{asset('frontend/galeri/'.$row->foto)}}" alt="">
+                                </div>
+                                <div class="team-one__content">
+                                    <div class="team-one__info">
+                                        <p class="team-one__sub-title">Co Founder</p>
+                                        <h3 class="team-one__name"><a href="team.html">{{$row->judul}}</a></h3>
+                                    </div>
+                                </div>
+                                <div class="team-one__social">
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-facebook"></i></a>
+                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="row">
-                        
-                        <div class="maine_action">
-
-                        @foreach($galeri_foto as $row)  
-                                                <div class="col-md-3">
-                                        
-                                                <div class="card-video">
-                                                <img src="{{asset('frontend/galeri/'.$row->foto)}}" alt="" />
-                                                         <!-- <h5><a href="">{{$row->judul}}</a></h5> -->
-                                                    
-                                   </div>
-                    </div>@endforeach  
-                    
-                                   </div>
-                          
-                    </div>         
-                    <div align="center">
+                    </div>
+                    @endforeach
                     {{ $galeri_foto->links() }}
 
-                    </div>
                 </div>
-            </section>                            
-           
+            </div>
+        </section>
+        <!--Team One End-->
             @endsection
