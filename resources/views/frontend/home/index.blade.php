2426c260782cc442bf5b08e54c1cf13f7a28773d
Ajifirlana <firlana89@gmail.com>
2023-12-28 12:35:11 +0700

    slider

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -39,27 +39,6 @@
                     </div>
 @endforeach
 
-                    <div class="swiper-slide">
-                        <div class="image-layer"
-                            style="background-image: url(frontend/assets/images/backgrounds/main-slider-1-3.jpg);"></div>
-                        <!-- /.image-layer -->
-
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-xl-12">
-                                    <div class="main-slider__content">
-                                        <h2 class="main-slider__title">Build Everything <br> with Passion</h2>
-                                        <p class="main-slider__text">There are many of passages of lorem Ipsum, but the
-                                            majori have suffered <br> alteration in some form.</p>
-                                        <div class="main-slider__btn-box">
-                                            <a href="about.html" class="thm-btn main-slider__btn">Discover more</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
 
                 </div>
 
